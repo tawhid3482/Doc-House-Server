794015f59dc7067fbacd749d52bea03c50e50991
saikat <tawhidulislam3482@gmail.com>
2024-03-10 11:50:35 +0600

    jwt working

--- index.js
+++ index.js
@@ -28,6 +28,45 @@ async function run() {
     const serviceCollection = client.db("docDb").collection("service");
     const srProductsCollection = client.db("docDb").collection("srProducts");
     const appointmentCollection = client.db("docDb").collection("appointment");
+    const usersCollection = client.db("docDb").collection("users");
+
+    // jwt
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // middleWare for jwt
+    const verifyToken = (req, res, next) => {
+      // console.log(req.headers);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(401).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+
+
 
     app.get("/doctors", async (req, res) => {
       const result = await doctorsCollection.find().toArray();
